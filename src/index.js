3ab7cc38e81c9889f47321d4b100980a68dc645c
Joel Lahtinen <joel.k.lahtinen@aalto.fi>
2025-01-30 11:52:42 +0200

    add english translation

--- src/index.js
+++ src/index.js
@@ -128,3 +128,16 @@ const getRandomGame = () => {
 
 }
 getRandomGame()
+
+function langFI() {
+	const html = document.documentElement;
+	html.lang = "fi";
+	html.classList.remove("inEnglish");
+	html.classList.add("inFinnish");
+}
+function langEN() {
+	const html = document.documentElement;
+	html.lang = "en";
+	html.classList.remove("inFinnish");
+	html.classList.add("inEnglish");
+}
