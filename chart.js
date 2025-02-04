dd3d9c063c0b1d560ab0860e92c636cc1675e040
Luukas Pörtfors <lajp@iki.fi>
2025-02-04 17:27:21 +0200

    Updates

---
+++ chart.js
@@ -0,0 +1,87 @@
+const getScores = () => {
+    fetch("https://backend-tb3t.onrender.com")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch leaderboard data");
+      }
+      return res.json();
+    })
+    .then((res) => {
+      setTableData(res);
+      setGuilds(grouped(res));
+    })
+    .catch((error) => {
+      console.error("ERROR WITH FETCHING LEADERBOARD DATA: " + error);
+    });
+  setTimeout(getScores, 10000);
+}
+
+const grouped = (data) => {
+    let result= []
+    data.forEach((s) => {
+      const guild = s.guild.toUpperCase().replace('TIK', 'TiK').replace('ATHENE', 'Athene').replace('PRODEKO', 'Prodeko').replace('INKUBIO', 'Inkubio');
+      const g = result.find(g => g.name === guild)
+      if(g === undefined) {
+        result.push({name: guild, scores: [s], average: 0, median: 0, total: 0})
+      } else {
+        g.scores.push(s)
+      }
+    })
+    result.forEach((g) => {
+      g.average = g.scores.reduce((acc, c) => acc + c.time, 0) / g.scores.length;
+      g.median = g.scores[Math.floor(g.scores.length / 2)].time;
+      g.total = g.scores.length;
+    })
+    return result;
+
+
+
+}
+
+
+const setTableData = (data) => {
+    if (data === undefined || data.length == 0)
+        return;
+
+    const container = document.getElementById("individual-results");
+    container.innerHTML = "";
+
+    data.forEach((d,i) => {
+        container.innerHTML += `
+        <tr class="border-white">
+						<th>${i + 1}</th>
+						<th>${d.name}</th>
+						<th>${d.guild}</th>
+						<th>${d.time/1000} s</th>
+				</tr>    
+        `
+
+    })
+
+}
+
+const setGuilds = (data) => {
+    if (data === undefined || data.length == 0)
+        return;
+
+    const container = document.getElementById("guild-results");
+    container.innerHTML = "";
+
+    data.forEach(d => {
+        container.innerHTML += `
+        <tr class="border-white">
+          <th>${d.name}</th>
+          <th>${d.total}</th>
+          <th>${d.average/1000} s</th>
+          <th>${d.median/1000} s</th>
+        </tr>   
+        `
+    })
+
+}
+
+
+
+
+
+getScores()
---
+++ index.js
@@ -0,0 +1,164 @@
+const ufo = document.querySelector(".ufo");
+
+ufo.style.transform = "translateY(500px) translateX(-1500px)";
+
+const ufoLoop = anime({
+    targets: ".ufo",
+    translateY: 25,
+    direction: "alternate",
+    loop: true,
+    autoplay: false,
+    easing: "easeInOutSine",
+    duration: 2000,
+});
+
+const ufoEntry = anime({
+    targets: ".ufo",
+    translateY: 0,
+    translateX: 0,
+    direction: "normal",
+    easing: "spring(1, 80, 13, 10)",
+    duration: 1000,
+    delay: 300,
+    loop: false,
+    complete: function (anim) {
+        ufo.style.transform = "translateY(0px) translateX(0px)";
+        const ufoLoop = anime({
+            targets: ".ufo",
+            translateY: 25,
+            direction: "alternate",
+            loop: true,
+            autoplay: true,
+            easing: "easeInOutSine",
+            duration: 2000,
+        });
+        ufoLoop.play();
+    },
+});
+
+/*
+anime({
+    targets: '.ufo',
+    translateY: 25,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine',
+    duration: 2000,
+});*/
+
+const lowerSky = anime({
+    targets: ".ufo-sky-lower",
+    translateY: 25,
+    direction: "alternate",
+    loop: true,
+    easing: "easeInOutSine",
+    duration: 1500,
+    delay: 300,
+});
+
+const upperSky = anime({
+    targets: ".ufo-sky-upper",
+    translateY: 25,
+    direction: "alternate",
+    loop: true,
+    easing: "easeInOutSine",
+    duration: 1700,
+    delay: 250,
+});
+
+anime({
+    targets: ".ufo-sky-lower-upscreen",
+    translateY: -25,
+    direction: "alternate",
+    loop: true,
+    easing: "easeInOutSine",
+    duration: 1300,
+    delay: 150,
+});
+
+anime({
+    targets: ".down-arrow",
+    translateY: 50,
+    direction: "alternate",
+    loop: true,
+    easing: "easeInOutSine",
+    duration: 500,
+});
+
+const updateDate = () => {
+    const target = new Date("2025-02-26 19:00:00 GMT+0300");
+    const now = new Date();
+
+    const dif = target.getTime() - now.getTime();
+
+    const days = Math.floor(dif / 86400000);
+    const hours = Math.round((dif % 86400000) / 3600000);
+    const minutes = Math.round(((dif % 86400000) % 3600000) / 60000);
+    const seconds = Math.round(((dif % 86400000) % 60000) / 1000);
+
+    document.getElementById("days").style.setProperty("--value", days);
+    document.getElementById("hours").style.setProperty("--value", hours);
+    document.getElementById("minutes").style.setProperty("--value", minutes);
+    document.getElementById("seconds").style.setProperty("--value", seconds);
+
+    requestAnimationFrame(updateDate);
+};
+
+updateDate();
+
+const games = {
+    bh: {
+        width: "300px",
+        height: "400px",
+    },
+    td: {
+        width: "1152px",
+        height: "648px",
+    },
+    kellotusSim: {
+        width: "300px",
+        height: "300px",
+    },
+};
+
+const startGame = (game) => {
+    var ifrm = document.createElement("iframe");
+    ifrm.setAttribute("src", "games/" + game + "/index.html");
+    ifrm.id = "gameFrame";
+    ifrm.style.width = games[game].width;
+    ifrm.style.height = games[game].height;
+    document.getElementById("gameMenu").style.display = "none";
+    document.getElementById("backToGameMenu").style.display = "flex";
+    document.getElementById("gameHolder").appendChild(ifrm);
+};
+
+const backToGameMenu = () => {
+    const gameFrame = document.getElementById("gameFrame");
+    document.getElementById("gameHolder").removeChild(gameFrame);
+    document.getElementById("backToGameMenu").style.display = "none";
+    document.getElementById("gameMenu").style.display = "flex";
+};
+
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+
+    if (window.scrollY > (window.innerHeight * 5) / 8) {
+        navbar.style.opacity = "1";
+    } else {
+        navbar.style.opacity = "0";
+    }
+});
+
+//smooth scrollaamine
+let anchorList = document.querySelectorAll('a[href^="#"]');
+anchorList.forEach((link) => {
+    link.onclick = function (e) {
+        e.preventDefault();
+
+        let destination = document.querySelector(this.hash);
+        destination.scrollIntoView({
+            behavior: "smooth",
+        });
+    };
+});
