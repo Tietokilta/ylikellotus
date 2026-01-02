e6e2afde3c2ea69c36f910caa58e49d819f804a2
bluelhf <ilari.suhonen@gmail.com>
2026-01-02 21:24:54 +0200

    feat: initial site setup

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div>
+      <main className="max-w-5xl mx-auto px-4 sm:px-6">
+        <p>no juuh tota se Tuukka Moilanen nii silt voi salee kysyy</p>
+        <p>kävisit kampuksella nii tuntisit</p>
+      </main>
+    </div>
+  );
+}
