cfd7d0a079999c7c1062a96591ef2ca3082b792e
bluelhf <ilari.suhonen@gmail.com>
2026-01-04 16:25:46 +0200

    feat(computer): add game

--- components/computer/windows/GameOne.tsx
+++ components/computer/windows/GameOne.tsx
@@ -2,6 +2,7 @@ export default function GameOne() {
     return (
         <>
             <img className="w-full h-full object-cover" alt="Game One" src="/assets/peli1.png" />
+            <iframe className="absolute top-[7%] w-[100%] h-[93%] border-black border-2" src="/assets/games/Blade%20of%20Työsoppari.html" title="Blade of Työsoppari"></iframe>
         </>
     )
 }
