290a6bcc830520a8ef9180656143a2ad8ca704dc
EeroMikkola <eerojuhanimikkola@gmail.com>
2026-01-04 16:59:33 +0200

    feat: make page bg color match asset

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import RatScene from "@/components/RatScene";
 export default function Home() {
   return (
     <div>
-      <main className="w-full mx-auto flex flex-col items-center">
+      <main className="w-full mx-auto flex flex-col items-center bg-[#f7f0f0]">
         <OfficeScene />
         <ComputerScene />
         <RatScene />
