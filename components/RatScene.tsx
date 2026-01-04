9261566f6525f06554a226aa8ee69dcf59aba62c
Saksunoob <olkkonen.saku@gmail.com>
2026-01-04 14:09:02 +0200

    feat: råtta scene

--- components/RatScene.tsx
+++ components/RatScene.tsx
@@ -2,8 +2,39 @@ import { Scene } from "./Scene";
 
 export default function RatScene() {
     return (
-        <Scene>
-            insert rat scene
+        <Scene className="w-[calc(100vh*11/16)] min-h-[0] h-[5vh]">
+            <div
+                style={{
+                    position: "relative",
+                    overflow: "visible",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center"
+                    }}
+                >
+                    <img
+                        src="/assets/rat.png"
+                        alt="Rat left"
+                        style={{ 
+                            width: "40%", 
+                            objectFit: "contain",
+                            transform: "translateY(-100%)"
+                        }}
+                    />
+                    <img
+                        src="/assets/osta1.png"
+                        alt="Rat right"
+                        style={{ 
+                            width: "40%", 
+                            objectFit: "contain",
+                            transform: "translateY(-70%)"
+                        }}
+                    />
+                </div>
+            </div>
         </Scene>
     )
 }
