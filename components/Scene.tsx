83f09b036c2eca3eb172ff127344188894e4608e
EeroMikkola <eerojuhanimikkola@gmail.com>
2026-01-04 17:43:10 +0200

    fix: scene overflow

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -6,7 +6,7 @@ type SceneProps = {
 export function Scene({ children, className = "" }: SceneProps) {
   return (
     <section
-      className={`w-full md:max-w-[1000px] h-full ${className}`}
+      className={`w-full md:max-w-[1000px] h-full overflow-hidden ${className}`}
     >
       <div className="top-16">
         {children}
