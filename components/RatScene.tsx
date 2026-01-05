4ae61943352c6cde1fd14ad7300feeb2cd1af983
Ilari Suhonen <ilari.suhonen@gmail.com>
2026-01-05 16:02:57 +0200

    chore(rat): convert style jsx to tailwind

--- components/RatScene.tsx
+++ components/RatScene.tsx
@@ -3,36 +3,16 @@ import { Scene } from "./Scene";
 export default function RatScene() {
     return (
         <Scene className="h-fit min-h-[0] overflow-visible">
-            <div
-                style={{
-                    position: "relative",
-                    overflow: "visible",
-                }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-						position: "absolute",
-						bottom: "0",
-                    }}
-                >
+            <div className="relative overflow-visible">
+                <div className="flex justify-center absolute bottom-0">
                     <img
                         src="/assets/rat.png"
                         alt="Rat left"
-                        style={{ 
-                            width: "40%", 
-                            objectFit: "contain",
-                        }}
-                    />
+                        className="w-[40%] object-contain"/>
                     <img
                         src="/assets/osta1.png"
                         alt="Rat right"
-                        style={{ 
-                            width: "40%", 
-                            objectFit: "contain",
-                        }}
-                    />
+                        className="w-[40%] object-contain"/>
                 </div>
             </div>
         </Scene>
