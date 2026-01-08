32f62ebfd32dbab1a7ecfd712928888a95edf1a5
bluelhf <ilari.suhonen@gmail.com>
2026-01-08 23:29:28 +0200

    fix(game-one): zoom out iframe so game looks better

--- components/computer/windows/GameOne.tsx
+++ components/computer/windows/GameOne.tsx
@@ -2,7 +2,9 @@ export default function GameOne() {
     return (
         <>
             <img className="w-full h-full object-cover" alt="Game One" src="/assets/peli1.png" />
-            <iframe className="absolute top-[7%] w-[100%] h-[93%] border-black border-2" src="/assets/games/Blade%20of%20Työsoppari.html" title="Blade of Työsoppari"></iframe>
+            <div className="absolute top-[7%] w-[100%] h-[93%]">
+                <iframe className="w-[400%] h-[400%] border-black border-2" style={{transform: 'scale(0.25)', transformOrigin: "0 0"}} src="/assets/games/Blade%20of%20Työsoppari.html" title="Blade of Työsoppari"></iframe>
+            </div>
         </>
     )
 }
