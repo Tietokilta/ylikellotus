6ac6cd0355615dbffe231c7fb63cd93d934ec8d0
bluelhf <ilari.suhonen@gmail.com>
2026-01-12 22:03:49 +0200

    feat(table): add text on page

--- components/TableScene.tsx
+++ components/TableScene.tsx
@@ -1,20 +1,30 @@
 import { Scene } from "./Scene";
+import {KIDE_URL} from "@/app/constants";
 
 
 export default function TableScene() {
     return (
         <Scene className="translate-y-[-32px] h-full">
             <div className="relative w-full min-w-[1600px] translate-x-[-50%] left-[50%]">
-                <span className="absolute w-[50%] h-[20%] translate-x-[27%] translate-y-[141%] origin-top-left rotate-[-13deg]">
-                    {/*
-                    <p className="text-red-500 text-xs md:text-3xl">
-                        Tervetuloa 40v synttäreiilleni<br />
-                        24.2. BMKilla <br />
-                        meillä on pallomeri<br />
-                        lämpimin terveisin:
+                <div data-balloon-spawnable="false" className="absolute left-[59%] top-[15%] w-[16.5%] h-[30%] origin-top-left rotate-[20deg]" style={{clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 42.5%, 14.1% 37.2%, 0% 23%)", containerType: "inline-size"}}>
+                    <p className={`font-normal text-[4.95cqw] text-justify font-serif`}>
+                        Hei,<br/><br/>
+                        Pahoittelut edellisestä viestistä.
+                        Työntekijä on nyt saanut ohjeistuksen käyttäytyä asiallisesti.
+                        Viestin tarkoituksena oli kutsua teidät Tietokillan neljäkymmentävuotisjuhliin
+                        Servin mökkiin Otaniemeen tänä helmikuun yhdentenätoista päivänä vuotta 2026.
+                        Saavuttehan paikalle viimeistään klo 20.00.
+                        <br/><br/>
+                        Lippu: Saatavilla <a className="text-blue-600 inline-block -m-2 p-2" href={KIDE_URL}>Kide.app</a>-sivulta.<br/>
+                        Teema: Byrokratia<br/>
+                        Pukukoodi: Haalarit ja Business Casual<br/>
+                        Paikka: Servin mökki, Otaniemi, Espoo.<br/>
+                        Aika: 11.2.2026 klo 19.00–02.00<br/>
+                        <br/>
+                        Ystävällisin terveisin,<br/>
+                        Tietokillan HR-tiimi
                     </p>
-                    */}
-                </span>
+                </div>
                 <img alt="Table" src="/assets/wide/pöytä.png" className="w-full h-auto" />
             </div>
         </Scene>
