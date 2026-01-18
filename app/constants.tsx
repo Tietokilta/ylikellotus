256920226c8c6573898b25c1f87bf3880a147279
bluelhf <ilari.suhonen@gmail.com>
2026-01-18 22:33:11 +0200

    chore(ad): use github attachment for video
    woo cdn!

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,3 +1,3 @@
 export const KIDE_URL = undefined; // "https://kide.app/events/<insert event>";
-export const VIDEO_URL = "https://pithos.lhf.blue/signed_download/f7bd1c35-d1cf-4331-bfc1-e0165ad77369?type_hint=video/mp4&signature=4b8d498a83a8760f92b09c683c97b894f330fcc0c14180b61d721a016db4bdc3";
+export const VIDEO_URL = "https://github.com/user-attachments/assets/75cdfa5c-f681-4301-8cad-059c2602d414";
 export const START_DATE = new Date(2026, 1, 11, 19, 0, 0);
