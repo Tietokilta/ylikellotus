3b23c8ecd632e3d442e2ec1c636863f2dbafab73
bluelhf <ilari.suhonen@gmail.com>
2026-01-18 22:32:47 +0200

    chore(responsiveness): make table scene text more responsive

--- components/TableScene.tsx
+++ components/TableScene.tsx
@@ -50,7 +50,7 @@ export default function TableScene() {
             { /* TODO: ensure this translateY is correct; it should prevent a blank space between the office and table scenes, even on ultrawide aspect ratios */}
             <div className="relative scene-body translate-x-[-50%] left-[50%]">
                 <div data-balloon-spawnable="false" className="absolute left-[59%] top-[15%] w-[16.5%] h-[30%] origin-top-left rotate-[20deg]" style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 42.5%, 14.1% 37.2%, 0% 23%)", containerType: "inline-size" }}>
-                    <p className={`text-[4.8cqw] text-justify font-serif`}>
+                    <p className={`text-[4.6cqw] md:text-[4.8cqw] text-justify font-serif`}>
                         {lang === "english" ? <EnglishText /> : <FinnishText />}<br/>
                         <span data-balloon-spawnable="false" className="absolute bottom-0 cursor-pointer text-blue-500 underline select-none pointer-events-auto" onClick={toggleLanguage}>
                         {lang === "english" ? "Käännä suomeksi" : "Switch to English"}
